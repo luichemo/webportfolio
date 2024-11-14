c54b4a2a11902d0a683f53b896131d3283d1a286
Luka Chemia <lchemia@aldagi.ge>
2024-11-14 15:37:40 +0400

    changing resp

--- dist/webportfolio/server/main.server.mjs
+++ dist/webportfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BJBCYGHA.mjs";import"./chunk-QHFVVD2D.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-YB5DZOGD.mjs";import"./chunk-QHFVVD2D.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
