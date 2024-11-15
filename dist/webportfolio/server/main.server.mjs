b02caf8b9fb9662ed5f7a297b9bec654db4dd960
Luka Chemia <lchemia@aldagi.ge>
2024-11-15 16:51:56 +0400

    about me done

--- dist/webportfolio/server/main.server.mjs
+++ dist/webportfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SHKZEGJF.mjs";import"./chunk-QHFVVD2D.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2SPQBOYQ.mjs";import"./chunk-QHFVVD2D.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
