0fd7600d532cad68ebfb1b45211302772cda00eb
Luka Chemia <lchemia@aldagi.ge>
2024-12-06 14:48:26 +0400

    add footer

--- dist/webportfolio/server/main.server.mjs
+++ dist/webportfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2BNPHO3G.mjs";import"./chunk-XIHJBNZS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-PWLXTRYE.mjs";import"./chunk-XIHJBNZS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
