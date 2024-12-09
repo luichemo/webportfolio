a08187fc5e77114da5b89d97a2db73fb28dadbdc
Luka Chemia <lchemia@aldagi.ge>
2024-12-09 12:56:06 +0400

    fixing responsive

--- dist/webportfolio/server/main.server.mjs
+++ dist/webportfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PWLXTRYE.mjs";import"./chunk-XIHJBNZS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-EJ2UK3L7.mjs";import"./chunk-XIHJBNZS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
